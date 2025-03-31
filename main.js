98d38e566c5e273aadb4f952a47175476610f70c
keetaeYang <keetae125@gmail.com>
2025-03-31 14:39:04 +0900

    r

---
+++ main.js
@@ -0,0 +1,14 @@
+
+function toggleMenu() {
+    document.getElementById("mobileNav").classList.toggle("show");
+}
+
+document.getElementById('lang-toggle').addEventListener('click', () => {
+    const elements = document.querySelectorAll('[data-kr]');
+    const isKR = document.documentElement.lang === 'ko';
+    document.documentElement.lang = isKR ? 'en' : 'ko';
+    elements.forEach(el => {
+        el.textContent = isKR ? el.dataset.en : el.dataset.kr;
+    });
+    document.getElementById('lang-toggle').textContent = isKR ? 'KR' : 'EN';
+});
