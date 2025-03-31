823e08dd9a4bc40fdad7b85bcea931c48ab3630c
keetaeYang <keetae125@gmail.com>
2025-03-31 13:47:56 +0900

    up

---
+++ main.js
@@ -0,0 +1,10 @@
+
+document.getElementById('lang-toggle').addEventListener('click', () => {
+    const elements = document.querySelectorAll('[data-kr]');
+    const isKR = document.documentElement.lang === 'ko';
+    document.documentElement.lang = isKR ? 'en' : 'ko';
+    elements.forEach(el => {
+        el.textContent = isKR ? el.dataset.en : el.dataset.kr;
+    });
+    document.getElementById('lang-toggle').textContent = isKR ? 'KR' : 'EN';
+});
